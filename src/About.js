97178d5c43c24bcc2411e90d8c5a9e9f8c6f40ed
bingery <470311811@qq.com>
2020-03-26 18:26:29 +0800

    yang

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// export default class About extends Component {
+//     render () {
+//         return <div>about</div>
+//     }
+// }
+
+export default function About() {
+    return <div>about</div>
+}
